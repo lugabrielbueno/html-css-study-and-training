c272a8248a29d162770c391cf11c2b09da31a5f7
Gabriel Bueno <gabriel-euro@hotmail.com.br>
2020-10-05 08:13:07 -0300

    menu apple

---
+++ full-stack-front-end/ux-ui/menu/script.js
@@ -0,0 +1,8 @@
+function abrirmenu(){
+    var menu = document.getElementById('menu-mobile');
+    if (menu.style.display == "none"){
+        menu.style.display = 'inline-block';
+    }else{
+        menu.style.display = "none";
+    }
+}
